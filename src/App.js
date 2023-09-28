5f2f6e350b5e72658833134892f5ed11f21fb152
Oluwanifemi <andyoged92@gmail.com>
2023-09-28 21:13:09 +0000

    practicing

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Job salary={90000} position= "Senior SDE" company="Amazon"        />
+
+
+    </div>
   );
+}
+
+const Job = (props) => {
+  return (
+    <div>
+    <h1>{props.salary}</h1>
+    <h1>{props.position}</h1>
+    <h1>{props.company}</h1>
+    
+
+
+    </div>
+  )
 }
 
 export default App;
