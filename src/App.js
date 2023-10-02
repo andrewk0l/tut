f85c8cecc4574e09bfa923f75b4f3f0552c603d7
Oluwanifemi <andyoged92@gmail.com>
2023-10-02 08:32:06 +0000

    fetching apis

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import './App.css';
-import { useState } from 'react';
+import {useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-const [count, setCount] = useState(0)
+const [excuse, setExcuse] = useState("")
 
-const increaseCount = () => {
-setCount(count + 1);
-}
 
-const decreaseCount = () => {
-setCount(count - 1);
+const fetchExcuse1 = () => {
+  axios.get("https://excuser-three.vercel.app/v1/excuse/family/").then((res) =>{
+    setExcuse(res.data[0].excuse)
+    console.log(res.data)
+  })  
 }
-
-const resetZero = () => {
-  setCount(count - count)
-}
+ 
 
 return (
     <div className="App">
-      {count}
-      <button onClick={increaseCount}>Increase</button>
-      <button onClick={decreaseCount}>Decrease</button>
-      <button onClick={resetZero}>Reset</button>
-      
-      {count}
+  <div> 
+    <h1>Generate an Excuse</h1>
+  </div>
+  <div><button >Party</button></div>
+  <div><button onClick={fetchExcuse1} >Family</button></div>
+  <div><button>Office</button></div>
+  
+  <p> {excuse} </p>
     </div>
   )
 
